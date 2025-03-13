bd223d85296dcf6dd74136bc29e7f00c4b33ebab
Naufal Yoga Pratama <naufaltino2113@gmail.com>
2025-03-13 14:29:44 +0700

    feat: Add and update ListItems page with connection string and CRUD operations
    - Corrected the connection string in ListItems.aspx.cs
    - Added TextBox controls for ID, Title, Genre, and Release in ListItems.aspx
    - Added buttons for Add, Delete, and Update operations in ListItems.aspx
    - Implemented DataShow method to display data in GridView

--- Modul6_Kel31/ListItems.aspx
+++ Modul6_Kel31/ListItems.aspx
@@ -9,7 +9,45 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <table>
+                <tr>
+                    <td>ID :</td>
+                    <td>
+                        <asp:TextBox ID="txtIdBuku" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Judul :</td>
+                    <td>
+                        <asp:TextBox ID="txtJudul" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Genre :</td>
+                    <td>
+                        <asp:TextBox ID="txtGenre" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Rilis :</td>
+                    <td>
+                        <asp:TextBox ID="txtRilis" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnAdd" runat="server" Text="ADD"
+                            OnClick="btnAdd_Click" />
+                        <asp:Button ID="btnDelete" runat="server"
+                            Text="DELETE" OnClick="btnDelete_Click" />
+                        <asp:Button ID="btnUpdate" runat="server"
+                            Text="UPDATE" OnClick="btnUpdate_Click" />
+                    </td>
+                </tr>
+            </table>
         </div>
+        <asp:GridView ID="GridViewJoin" runat="server">
+        </asp:GridView>
     </form>
 </body>
 </html>
--- Modul6_Kel31/ListItems.aspx.cs
+++ Modul6_Kel31/ListItems.aspx.cs
@@ -4,19 +4,65 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Modul6_Kel31
 {
-	public partial class ListItems : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    public partial class ListItems : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
 
-		}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = "Data Source=LAPTOP-R9HQTSI4\\SQLEXPRESS; Initial Catalog=mod3_perc2_kel31; Integrated Security=True";
+            con.Open();
+            if (!Page.IsPostBack)
+            {
+                DataShow();
+            }
+        }
+
+        public void DataShow()
+        {
+            ds = new DataSet();
+            cmd.CommandText = "Select * from Buku";
+            cmd.Connection = con;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            GridViewJoin.DataSource = ds;
+            GridViewJoin.DataBind();
+            con.Close();
+        }
 
         protected void btnAdd_Click(object sender, EventArgs e)
         {
+            dt = new DataTable();
+            cmd.CommandText = "INSERT INTO Buku(ID_Buku, Judul, Genre, Rilis) VALUES('" + txtIdBuku.Text + "', '" + txtJudul.Text.ToString() + "', '" + txtGenre.Text.ToString() + "','" + txtRilis.Text + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
 
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            cmd.CommandText = "DELETE FROM Buku WHERE ID_Buku = '" + txtIdBuku.Text + "'";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            cmd.CommandText = "UPDATE Buku SET Judul = '" + txtJudul.Text + "', Genre = '" + txtGenre.Text + "', Rilis = '" + txtRilis.Text + "' WHERE ID_Buku = '" + txtIdBuku.Text + "'";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
         }
     }
 }
