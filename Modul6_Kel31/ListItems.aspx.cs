5b243a1d9b6fe7986df34703895ead93107fc401
Naufal Yoga Pratama <naufaltino2113@gmail.com>
2025-03-13 14:16:25 +0700

    first commit

---
+++ Modul6_Kel31/ListItems.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Modul6_Kel31
+{
+	public partial class ListItems : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
